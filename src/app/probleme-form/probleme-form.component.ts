88f42f9f5a53fd3299495393103264da072c246e
allison chidiac <zedAM1826232@gmail.com>
2022-04-11 17:25:25 -0400

    commit4

---
+++ src/app/probleme-form/probleme-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-probleme-form',
+  templateUrl: './probleme-form.component.html',
+  styleUrls: ['./probleme-form.component.css']
+})
+export class ProblemeFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
